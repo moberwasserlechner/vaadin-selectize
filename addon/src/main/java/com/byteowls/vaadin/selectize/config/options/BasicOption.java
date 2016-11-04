c33b80400fc6b68d2144ee7eb9a462639004b7c0
Michael Oberwasserlechner <m.oberwasserlechner@mum-software.com>
2016-11-04 11:06:08 +0100

    Add annotations to BasicOption bean

--- addon/src/main/java/com/byteowls/vaadin/selectize/config/options/BasicOption.java
+++ addon/src/main/java/com/byteowls/vaadin/selectize/config/options/BasicOption.java
@@ -1,8 +1,15 @@
 package com.byteowls.vaadin.selectize.config.options;
 
+import com.byteowls.vaadin.selectize.config.annotation.SelectizeOptionLabel;
+import com.byteowls.vaadin.selectize.config.annotation.SelectizeOptionSearch;
+import com.byteowls.vaadin.selectize.config.annotation.SelectizeOptionValue;
+
 public class BasicOption {
 
+    @SelectizeOptionValue
     private Object value;
+    @SelectizeOptionLabel
+    @SelectizeOptionSearch
     private String text;
 
     public BasicOption(Object value, String text) {
@@ -25,6 +32,10 @@ public class BasicOption {
     public void setText(String text) {
         this.text = text;
     }
-    
+
+    @Override
+    public String toString() {
+        return "BasicOption [value=" + value + ", text=" + text + "]";
+    }
 
 }
